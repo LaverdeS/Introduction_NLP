153d4127cb81ae5be19f26112a065c889ebbd8e2
Ademola Eric Adewumi <demoric102@gmail.com>
2020-07-06 21:29:05 +0200

    Update preprocessor.py

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -4,39 +4,64 @@ import re
 from tqdm import tqdm_notebook as tqdm
 import string
 from collections import Counter
-from sklearn.feature_extraction.text import TfidfTransformer
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import Counter
 import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+from numpy import array
+import numpy as np
+import csv
 
+#preprocessing
 path = 'semeval2016-task6-trainingdata.txt'
 df = pd.read_csv(path, '\t', encoding='mac_roman')
-pd_list = df['Tweet'].values.tolist()
-tokenizer = RegexpTokenizer(r'\w+')
+pd_list=df[['Tweet','Stance']].values.tolist()
+tokenizer=RegexpTokenizer(r'\w+')
 pattern = '[^A-Za-z]+'
-pd_list = df['Tweet'].apply(lambda x: re.sub(pattern, ' ', x))
-pd_list = pd_list.apply(lambda x: tokenizer.tokenize(x.lower()))
-targets = list(set(df['Target']))
+pd_list=df['Tweet'].apply(lambda x: re.sub(pattern, ' ', x))
+pd_list=pd_list.apply(lambda x: tokenizer.tokenize(x.lower()))
+stances=df['Stance'].apply(lambda x: tokenizer.tokenize(x.lower()))
+targets=list(set(df['Target']))
+tweet_stance=pd.concat([pd_list,stances], axis=1, sort=False)
 
+search="Climate Change is a Real Concern"
 for target in targets:
-    target = df['Target'] == 'Legalization of Abortion'
-    df_target = pd_list[target]
-df_target
+  target = df['Target'] == search
+  df_target=tweet_stance[target]
+print(df_target)
+
+
+#Feature Engineering
+tweet_list=[]
+bow = list()
+word=list()
+tweet_list = [' '.join(x) for x in df_target['Tweet']]
+for tweet in tweet_list:
+  tweet_set=set(tweet.split(" "))
+  bow.append(tweet_set)
+  vocabulary = [item for tweet_set in bow for item in tweet_set]
+numOfWords = dict.fromkeys(vocabulary, 0)
+for word in vocabulary:
+    numOfWords[word] += 1
+bow=vocabulary
+dic=set(vocabulary)
+dict_list=list(dic)
 
 vectorizer = CountVectorizer()
-tweet_list = []
-frequency = [Counter(tweet) for tweet in df_target]
-set_all = set()
-new_list = [' '.join(x) for x in df_target]
-vec_transform = vectorizer.fit_transform(new_list)
-for tweet in df_target:
-    tweet_set = set(tweet)
-    set_all = set_all.union(tweet_set)
-cv = pd.DataFrame(vec_transform.toarray())
-transform = TfidfTransformer()
-trans = transform.fit(vec_transform)
-cv.to_csv('legalization_of_abortion_bow.tsv', sep='\t', encoding='utf-8')
-with open('legalization_of_abortion_tfidf.pkl', 'wb') as f:
-    pickle.dump(trans.idf_, f)
+tf_idf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+stance=df_target['Stance']
+
+#Bag Of Words
+bag_of_words=vectorizer.fit_transform(tweet_list).toarray()
+
+#Tf-IDF
+word_count_vector=vectorizer.fit_transform(tweet_list)
+feature_names = vectorizer.get_feature_names()
+data_frame = pd.DataFrame(word_count_vector.T.todense(), index=feature_names)
+transposed_cv=data_frame.transpose()
+transposed_cv.to_csv('climate_tfidf.tsv', sep='\t', encoding='utf-8')
+stance.to_csv('climate_stance.tsv', sep='\t', encoding='utf-8')
